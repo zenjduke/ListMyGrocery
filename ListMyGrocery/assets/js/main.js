be59743a3e5c59b8668eb51acbc91997d47b4212
desaigner <sai@desaign.me>
2018-05-21 18:44:20 -0500

    adding sai branch

---
+++ ListMyGrocery/assets/js/main.js
@@ -0,0 +1,57 @@
+
+//menu
+
+function openLeftMenu() {
+    document.getElementById("leftMenu").style.display = "block";
+}
+function closeLeftMenu() {
+    document.getElementById("leftMenu").style.display = "none";
+}
+function openRightMenu() {
+    document.getElementById("rightMenu").style.display = "block";
+}
+function closeRightMenu() {
+    document.getElementById("rightMenu").style.display = "none";
+}
+
+
+/* ===========================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================
+SAI'D==NOTE:==STILL==WORKING==ON==IT
+===============================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================
+*/
+// Change style of navbar on scroll
+
+//window.onscroll = function() {myFunction()};
+//function myFunction() {
+//    var navbar = document.getElementById("myNavbar");
+//    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+//        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white" + "w3-center" + " w3-padding-large" + " w3-xlarge" + " w3-wide" + "w3-animate-opacity";
+//    } else {
+//        navbar.classList.remove("w3-bar w3-card w3-animate-top w3-white")
+//		navbar.className =  "w3-center" + " w3-padding-large" + " w3-xlarge" + " w3-wide" + "w3-animate-opacity" + "w3-black";
+//    }
+//}
+//
+//// Used to toggle the menu on small screens when clicking on the menu button
+//
+//function toggleFunction() {
+//    var x = document.getElementById("navDemo");
+//    if (x.className.indexOf("w3-show") == -1) {
+//        x.className += " w3-show";
+//    } else {
+//        x.className = x.className.replace(" w3-show", "");
+//    }
+//}
+
+//window.onscroll = function() {myFunction()};
+//
+//var navbar = document.getElementById("processBar");
+//var sticky = navbar.offsetTop;
+//
+//function myFunction() {
+//  if (window.pageYOffset >= sticky) {
+//    navbar.classList.add("sticky")
+//  } else {
+//    navbar.classList.remove("sticky");
+//  }
+//}
