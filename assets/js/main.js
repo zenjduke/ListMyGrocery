d5d2000e3fe94224ee492dc0bb62253910662ea1
zenjduke <zennajduke@gmail.com>
2018-05-31 19:39:22 -0500

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,13 +12,6 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-var myIngredientObject = {
-  amount: [],
-  unit: [],
-  name: [] 
-};
-
-var myIngredients = [];
 //menu
 
 function openLeftMenu() {
@@ -54,17 +47,25 @@ var ingredientObject = {
   name: [],
 }
 
+// ---------myIngredient OBJECT For Added Recipes
+var myIngredientObject = {
+  amount: [],
+  unit: [],
+  name: [] 
+};
+
+var myIngredients = [];
+
 // ---------RecipeAPP OBJECT
 
 var recipeApp = {
 
+    // ----- RECIPE INFORMATION MODAL 
     openRecipeSearch: function(){
-        // ------------- RECIPE INFORMATION MODAL 
+        
     
       // Get the modal
       var modal = document.getElementById('mySearchModal');
-  
-      console.log("made it");
   
       // Get the button that opens the modal
       var btn = document.getElementById("complexSearch");
@@ -208,6 +209,41 @@ var recipeApp = {
           modal.style.display = "none";
         }
     
+    },
+
+     // ----- USER ADDED RECIPES MODAL 
+     showUserAddedRecipes: function(){
+    
+      // Get the modal
+      var modal = document.getElementById('addedRecipesModal');
+  
+      // Get the button that opens the modal
+      var btn = document.getElementById("addedRecipes");
+  
+      // Get the <span> element that closes the modal
+      var span = document.getElementsByClassName("addedRecipesClose")[0];
+  
+      // When the user clicks on the button, open the modal 
+  
+      modal.style.display = "block";
+      
+  
+      // When the user clicks on <span> (x), close the modal
+      span.onclick = function() {
+          modal.style.display = "none";
+      }
+
+      closebtn.onclick = function() {
+        modal.style.display = "none";
+      }
+  
+      // When the user clicks anywhere outside of the modal, close it
+      
+      if (event.target == modal) {
+            modal.style.display = "none";
+          }
+          console.log("showing recipes added by user");
+      
     },
 
       // listIngredients will create buttons from ingredientArray, or buttons for eahc ingredient in one clicked recipe. From here, user can choose what needs to be added to their personal list. (And possibly change amounts?)
@@ -692,11 +728,19 @@ $(document).on('click','.learnMore',function(){
   })
   });
 
+  //-------------OPEN USER ADDED RECIPES MODAL
+  $(document).on('click','#addedRecipes',function(){
+    event.preventDefault();
+    recipeApp.showUserAddedRecipes();
+  });
+
+  //-------------OPEN SEARCH NEW RECIPE MODAL
   $(document).on('click','#complexSearch',function(){
     event.preventDefault();
     recipeApp.openRecipeSearch();
   });
 
+  //-------------OPEN SEARCH BY INGREDIENT MODAL
   $(document).on('click','.ingredientButton',function(){
     event.preventDefault();
     recipeApp.openIngredientSearch();
@@ -709,11 +753,15 @@ $(document).on('click','.learnMore',function(){
     recipeApp.openAddRecipe();
   });
 
+  //-------------SEARCH NEW RECIPES
+
   $(document).on('click','#recipe-search',function(){
     event.preventDefault();
     $("#recipe-view").empty();
     recipeApp.searchRecipeComplex();
   });
+
+  //-------------SEARCH RECIPES USING INGREDIENTS YOU HAVE
 
   $(document).on('click','#ingredient-search',function(){
     event.preventDefault();
@@ -900,6 +948,6 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey) {
   console.log(recipeType);
   console.log(recipeIngredients);
 
-  $("#recipe-table > tbody").append("<tr id = "+myIngredients+"><td>" + recipeName + "</td><td>" + recipeMin + " minutes</td><td>" + recipeDiet + "</td><td>" + recipeType + "</td><td>" + recipeIngredients + "</td></tr>");
+  $("#recipe-table > tbody").append("<tr class='w3-hover-green'"+myIngredients+"><td>" + recipeName + "</td><td>" + recipeMin + " minutes</td><td>" + recipeDiet + "</td><td>" + recipeType + "</td><td>" + recipeIngredients + "</td></tr>");
 
 });
